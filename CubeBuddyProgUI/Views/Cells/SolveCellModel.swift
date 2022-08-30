2f31fd2c9a9039139e9179f685e98cb2e68fdf8f
iGFarr <isaacgfarr@live.com>
2022-08-30 04:42:27 -0400

    Removed all mentions of self where possible in SolveCellModel; cleaner look

--- CubeBuddyProgUI/Views/Cells/SolveCellModel.swift
+++ CubeBuddyProgUI/Views/Cells/SolveCellModel.swift
@@ -16,7 +16,7 @@ class SolveCellModel: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.backgroundColor = .clear
+        backgroundColor = .clear
         setup()
         CBViewCreator.createCellSeparator(for: self)
     }
@@ -26,24 +26,24 @@ class SolveCellModel: UITableViewCell {
     }
     
     private func setup() {
-        self.solveTimeLabel.numberOfLines = 1
-        self.puzzleLabel.numberOfLines = 1
+        solveTimeLabel.numberOfLines = 1
+        puzzleLabel.numberOfLines = 1
         
-        self.contentView.addSubview(self.stackView)
-        self.contentView.addSubview(self.puzzleLabel)
-        self.stackView.addArrangedSubview(self.solveTimeLabel)
-        self.stackView.addArrangedSubview(self.scrambleLabel)
+        contentView.addSubview(stackView)
+        contentView.addSubview(puzzleLabel)
+        stackView.addArrangedSubview(solveTimeLabel)
+        stackView.addArrangedSubview(scrambleLabel)
         
         NSLayoutConstraint.activate([
-            self.puzzleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -CBConstants.UIConstants.doubleInset),
-            self.puzzleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: CBConstants.UIConstants.defaultInsets),
+            puzzleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -CBConstants.UIConstants.doubleInset),
+            puzzleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: CBConstants.UIConstants.defaultInsets),
 
-            self.stackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: CBConstants.UIConstants.defaultInsets),
-            self.stackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -CBConstants.UIConstants.stackViewTrailingInset),
-            self.stackView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: CBConstants.UIConstants.defaultInsets),
-            self.stackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -CBConstants.UIConstants.defaultInsets),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: CBConstants.UIConstants.defaultInsets),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -CBConstants.UIConstants.stackViewTrailingInset),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: CBConstants.UIConstants.defaultInsets),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -CBConstants.UIConstants.defaultInsets),
             
-            self.scrambleLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor)
+            scrambleLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor)
         ])
     }
 }
